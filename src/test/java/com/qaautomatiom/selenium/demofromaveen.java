7ded7cda1c9b079706192b1ccd24108de90470f6
AcquireQAskills <38130078+AcquireQAskills@users.noreply.github.com>
2018-08-01 22:26:30 +0530

    maveen Demo

---
+++ src/test/java/com/qaautomatiom/selenium/demofromaveen.java
@@ -0,0 +1,12 @@
+package com.qaautomatiom.selenium;
+
+import org.testng.annotations.Test;
+
+public class demofromaveen
+{
+    @Test
+	public void testhello()
+	{
+		System.out.println("welcome to maveen");
+	}
+}
